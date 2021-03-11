5fd8841765de704d4e3522316a15bc26a9dc9c58
Sander Byrkjeland <sunbyr@gmail.com>
2021-03-11 15:06:57 +0100

    wip, auth done, jwt done

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+	const token = req.header("Authorization");
+
+	if (!token) {
+		return res.status(401).json({ message: "No Token, Authorization denied" });
+	}
+	try {
+		jwt.verify(token, config.get("secretToken"), (error, decoded) => {
+			if (error) {
+				return res.status(400).json({ message: "Token is not valid" });
+			} else {
+				req.user = decoded.user;
+				next();
+			}
+		});
+	} catch (error) {
+		res.status(500).json({ message: "server error" });
+	}
+};
